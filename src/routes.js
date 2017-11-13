4f277a0c3bb0be1ab1b67437708fb38633bcf406
sirjmkitavi <kitavimjoseph@gmail.com>
2017-11-13 23:04:25 +0300

    Create PrivateRoute

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
+import {
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router';
 import App from './components/App';
 import LoginPage from './components/login/LoginPage';
+import HomePage from './components/home/HomePage';
 
 const Routes = () => (
   <App>
     <Switch>
-      <Route exact path="/" component={LoginPage}/>
+      <PrivateRoute exact path="/" component={HomePage} />
+      <PrivateRoute exact path="/home" component={HomePage} />
+      <Route exact path="/login" component={LoginPage} />
     </Switch>
   </App>
 );
+
+let token = localStorage.getItem('Authorization')
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    token ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }}/>
+    )
+  )}/>
+)
 
 export default Routes;
