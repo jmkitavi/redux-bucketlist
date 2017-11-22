6faa432f39a4170a6211092856ad28e1915635b8
sirjmkitavi <kitavimjoseph@gmail.com>
2017-11-22 18:07:04 +0300

    Update header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,19 +5,21 @@ import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
-    console.log(this.props)
     return (
       <nav className="navbar navbar-inverse">
-          <ul className="nav navbar-nav">
-            <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
-            <li><NavLink to="/bucketlists" activeClassName="active">Bucket Lists</NavLink></li>
-            <li><NavLink to="/signup" activeClassName="active">Sign Up</NavLink></li>
-            {this.props.user ? (
-            <li><NavLink to="/logout" activeClassName="active">Log Out</NavLink></li>
-            ):(
-              <li><NavLink to="/login" activeClassName="active">Log in</NavLink></li>
-            )}
+          {this.props.user ? (
+            <ul className="nav navbar-nav">
+              <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+              <li><NavLink to="/bucketlists" activeClassName="active">Bucket Lists</NavLink></li>
+              <li><NavLink to="/logout" activeClassName="active">Log Out</NavLink></li>
           </ul>
+          ):(
+            <ul className="nav navbar-nav">
+              <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+              <li><NavLink to="/login" activeClassName="active">Log in</NavLink></li>
+              <li><NavLink to="/signup" activeClassName="active">Sign Up</NavLink></li>
+            </ul>
+          )}
       </nav>
     )
   }
