dc6133616084c7c1417daad0cb237bc5536d8fc9
sirjmkitavi <kitavimjoseph@gmail.com>
2018-01-10 19:17:46 +0300

    remove unused initialState

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
-export default function configureStore(initialState) {
+export default function configureStore() {
   return createStore(
     rootReducer,
     composeWithDevTools(
