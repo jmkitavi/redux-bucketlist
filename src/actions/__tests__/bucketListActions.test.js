3ce3b1e13ddb132594dc7126f8a84fc61c4fca38
sirjmkitavi <kitavimjoseph@gmail.com>
2018-01-25 00:03:02 +0300

    more bucketlist actions tests

--- src/actions/__tests__/bucketListActions.test.js
+++ src/actions/__tests__/bucketListActions.test.js
@@ -10,6 +10,15 @@ jest.mock('../../api/bucketlistApi', () => ({
   fetchBucketlists: () => Promise.resolve([
     { bucketlist_id: 1, title: 'Amazing', description: '', items: [] }
   ]),
+  saveBucketlist: (bucketlist) => Promise.resolve(
+    { bucketlist_id: 1, title: 'Amaze balls', description: '', items: [] }
+  ),
+  editBucketlist: (bucketlist) => Promise.resolve(
+    { bucketlist_id: 1, title: 'Amazed', description: '', items: [] }
+  ),
+  deleteBucketlist: (bucketlist) => Promise.resolve(
+    { bucketlist_id: 1, title: 'Amazed', description: '', items: [] }
+  )
 }))
 
 describe('BucketList Actions Tests', () => {
@@ -27,7 +36,43 @@ describe('BucketList Actions Tests', () => {
       })
   })
 
+  it('should dispatch SAVE_BUCKETLIST_SUCCESS when creating a bucketlist', () => {
+      const expectedActions = [
+        { type: types.SAVE_BUCKETLIST_SUCCESS,
+          bucketlist: { bucketlist_id: 1, title: 'Amaze balls', description: '', items: [] }
+        }
+      ];
+      const store = mockStore({ bucketlist: [] })
+      return store.dispatch(bucketlistActions.saveBucketlist(expectedActions[0].bucketlist))
+        .then(() => {
+          expect(store.getActions()).toEqual(expectedActions)
+        })
+    })
 
+  it('should dispatch EDIT_BUCKETLIST_SUCCESS when updating a bucketlist', () => {
+      const expectedActions = [
+        { type: types.EDIT_BUCKETLIST_SUCCESS,
+          bucketlist: { bucketlist_id: 1, title: 'Amazed', description: '', items: [] }
+        }
+      ]
+      const store = mockStore({ bucketlists: [] })
+      return store.dispatch(bucketlistActions.editBucketlist(expectedActions[0].bucketlist))
+        .then(() => {
+          expect(store.getActions()).toEqual(expectedActions)
+        })
+    })
 
+  it('should dispatch DELETE_BUCKETLIST_SUCCESS when deleting a bucketlist', () => {
+      const expectedActions = [
+        { type: types.DELETE_BUCKETLIST_SUCCESS,
+          bucketlist: { bucketlist_id: 1, title: 'Amazed', description: '', items: [] }
+        },
+      ];
+      const store = mockStore({ bucketlists: [] })
+      store.dispatch(bucketlistActions.deleteBucketlist(expectedActions[0].bucketlist))
+        .then(() => {
+          expect(store.getActions().type).toEqual(expectedActions.type);
+        })
+    })
 })
 
